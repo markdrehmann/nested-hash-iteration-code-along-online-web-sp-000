e37ec7ab154286f1e891249290254c5661fc5810
Mark Drehmann <markdrehmann@gmail.com>
2020-02-03 21:57:03 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,6 +16,10 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each do |person, details_hash|
-    binding.pry
+    if person == "Freddy Mercury"
+      details_hash.each do |attribute, data|
+        binding.pry
+      end
+    end
   end
 end
